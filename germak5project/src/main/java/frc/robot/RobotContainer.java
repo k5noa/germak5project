6706c335325e5760c403d99bac651dddd41ab551
gem-what <gavinr@vandenrobotics.com>
2025-09-09 16:28:33 -0700

    Update RobotContainer.java

--- germak5project/src/main/java/frc/robot/RobotContainer.java
+++ germak5project/src/main/java/frc/robot/RobotContainer.java
@@ -25,7 +25,6 @@ public class RobotContainer {
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
-//error
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
