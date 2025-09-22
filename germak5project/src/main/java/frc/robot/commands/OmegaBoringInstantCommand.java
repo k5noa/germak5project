b48698ab04e131852c0b446c459fed0e5ec103d4
gem-what <gavinr@vandenrobotics.com>
2025-09-22 16:17:54 -0700

    practice complete i think :3
    err i hope

--- germak5project/src/main/java/frc/robot/commands/OmegaBoringInstantCommand.java
+++ germak5project/src/main/java/frc/robot/commands/OmegaBoringInstantCommand.java
@@ -4,18 +4,29 @@
 
 package frc.robot.commands;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+
 import frc.robot.subsystems.SubsystemLimbusGachaSlop;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class OmegaBoringInstantCommand extends InstantCommand {
+
+  private SubsystemLimbusGachaSlop boringFriends;
+
   public OmegaBoringInstantCommand() {
+
+    boringFriends = new SubsystemLimbusGachaSlop();
+
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    boringFriends.StopLimbusMotor();
+  }
 }
